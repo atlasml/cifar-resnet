315cfc621e320a6e1bbea271ac5b6471321795fb
RJT1990 <rj-taylor@live.co.uk>
2019-06-11 14:10:32 +0100

    changing function names a little

--- hubconf.py
+++ hubconf.py
@@ -1,6 +1,6 @@
 dependencies = ['torch', 'torchvision']
 
 from model import cifar_srm_resnet32
-from sotabench.vision.image_classification import evaluate_cifar10
+from sotabench.vision.image_classification.cifar10 import evaluate_cifar10
 
 evaluate_cifar10(model=cifar_srm_resnet32(pretrained=True))
