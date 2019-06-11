2dc7c21905066d08b4f1cc07337f6ef0bdf1ba8e
RJT1990 <rj-taylor@live.co.uk>
2019-06-11 13:10:35 +0100

    Test model

---
+++ hubconf.py
@@ -0,0 +1,6 @@
+dependencies = ['torch', 'torchvision']
+
+from model import cifar_srm_resnet32
+from sotabench.vision.image_classification import evaluate_cifar10
+
+evaluate_cifar10(model=cifar_srm_resnet32(pretrained=True))
