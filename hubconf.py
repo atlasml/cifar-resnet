62665302b55262a81314bd54ba3baf2ad2c462a0
Ross Taylor <rj-taylor@live.co.uk>
2019-06-14 14:18:53 +0100

    Update hubconf.py with more paper metadata

--- hubconf.py
+++ hubconf.py
@@ -4,4 +4,9 @@ from model import cifar_srm_resnet32
 from sotabench.image_classification import cifar10
 
 def sotabench():
-    cifar10.benchmark(model=cifar_srm_resnet32(pretrained=True))
+    cifar10.benchmark(
+        model=cifar_srm_resnet32(pretrained=True),
+        paper_model_name='SRM ResNet 32',
+        paper_arxiv_id='1903.10829',
+        paper_pwc_id='srm-a-style-based-recalibration-module-for'
+    )
