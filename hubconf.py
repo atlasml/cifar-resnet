1157292212fc80f819e1bbf1bd9b7b6a40900099
rstojnic <rstojnic@users.noreply.github.com>
2019-06-18 09:47:40 +0100

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -3,7 +3,7 @@ dependencies = ['torch', 'torchvision']
 from model import cifar_srm_resnet32
 from sotabench.image_classification import cifar10
 
-def sotabench():
+def benchmark():
     cifar10.benchmark(
         model=cifar_srm_resnet32(pretrained=True),
         paper_model_name='SRM ResNet 32',
