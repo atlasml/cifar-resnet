d28d1c30b0611dde15908afa978b3fa6d1d55eca
Ross Taylor <rj-taylor@live.co.uk>
2019-06-26 16:59:50 +0100

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -1,12 +1,9 @@
-dependencies = ['torch', 'torchvision']
-
 from model import cifar_srm_resnet32
-from sotabench.image_classification import cifar10
+from sotabench.image_classification import CIFAR10
 
-def benchmark():
-    cifar10.benchmark(
-        model=cifar_srm_resnet32(pretrained=True),
-        paper_model_name='SRM ResNet 32',
-        paper_arxiv_id='1903.10829',
-        paper_pwc_id='srm-a-style-based-recalibration-module-for'
-    )
+CIFAR10.benchmark(
+    model=cifar_srm_resnet32(pretrained=True),
+    paper_model_name='SRM ResNet 32',
+    paper_arxiv_id='1903.10829',
+    paper_pwc_id='srm-a-style-based-recalibration-module-for'
+)
